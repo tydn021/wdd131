27d5a4161134023a85a259876d6e112c2855e551
tydn021 <tnelson9@byupathway.edu>
2026-01-19 09:18:53 -0800

    jan 19 2026

--- week02/scripts/debugging.js
+++ week02/scripts/debugging.js
@@ -1,25 +1,45 @@
-// const radiusOutput = document.getElementById('radius');
-// const areaOutput = document.querySelector('#area');
+const radiusOutput = document.getElementById('radius');
+const areaOutput = document.querySelector('#area');
 
-// let area = 0;
-// const PI = 3.14159;
+let area = 0;
+const PI = 3.14159;
 
-// let radius = 10;
-// area = PI * radius * radius;
-// radiusOutput.textContent = radius;
-// areaOutput.textContent = area;
+let radius = 10;
+area = PI * radius * radius;
+radiusOutput.textContent = radius;
+areaOutput.textContent = area;
 
-// radius = 20;
-// area = PI * radius * radius;
-// radiusOutput.textContent = radius;
-// areaOutput.textContent = area;
+radius = 20;
+area = PI * radius * radius;
+radiusOutput.textContent = radius;
+areaOutput.textContent = area;
 
-const DAYS = 6;
-const LIMIT = 39;
-let studentReport = [11, 42, 33, 64, 27, 33, 44]
+// const DAYS = 6;
+// const LIMIT = 39;
+// let studentReport = [11, 42, 33, 64, 27, 33, 44]
 
-for (let i = 0; i < studentReport.length; i++){
-    if (studentReport[i] < LIMIT) {
-        console.log(studentReport[i]);
-    }
-}
+// for (let i = 0; i < studentReport.length; i++){
+//     if (studentReport[i] < LIMIT) {
+//         console.log(studentReport[i]);
+//     }
+// }
+
+// let i = 0;
+// while (i < studentReport.length) {
+//     if (studentLength[i] < LIMIT) {
+//         console.log(studentReport[i])
+//     }
+//     i++
+// }
+
+// studentReport.forEach(function (item) {
+//     if (item < LIMIT) {
+//         console.log(item)
+//     }
+// });
+
+// for (let i in studentReport) {
+//     if (studentReport[i] < LIMIT) {
+//         console.log(studentReport[i]);
+//     }
+// }
