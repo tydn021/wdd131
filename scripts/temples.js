6b3ac21758a0d80b173b38dbaaccdf585abec3ac
tydn021 <tnelson9@byupathway.edu>
2026-01-24 10:55:26 -0800

    svg added

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,4 +4,7 @@ const navigation = document.querySelector(".navigation");
 hamButton.addEventListener("click", () => {
 	navigation.classList.toggle("open");
 	hamButton.classList.toggle("open");
+
+	const isOpen = hamButton.classList.contains("open");
+	hamButton.setAttribute("aria-expanded", isOpen);
 });
